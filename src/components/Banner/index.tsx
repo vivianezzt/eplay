8ab2c7507d987b5ddaf5e5fe1e023916a16be645
Viviane <vivianeaguiarc@outlook.com>
2025-01-23 10:55:05 -0300

    feat: add components ButtonContainer, tag, ButtonLink

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,13 +1,28 @@
 import { Image, Title, Prices } from './styles'
 import bannerImg from '../../assests/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import { ButtonLink } from '../Button/styles'
 
 const Banner = () => (
   <Image style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
-      <Title>Marvel&apos;S Spider-Man: Morales PS$ & PS5</Title>
-      <Prices>
-        De <span>R$ 250,00</span> <br /> por apenas R$ 99,90
-      </Prices>
+      <Tag size="big">Destaque do dia</Tag>
+      <div>
+        <Title>
+          Marvel&apos;S Spider-Man: <br />
+          Morales PS$ & PS5
+        </Title>
+        <Prices>
+          De <span>R$ 250,00</span> <br /> por apenas R$ 99,90
+        </Prices>
+      </div>
+      <ButtonLink
+        type="link"
+        to="/produto"
+        title="Clique aqui para aproveitar esta oferta"
+      >
+        Aproveitar
+      </ButtonLink>
     </div>
   </Image>
 )
